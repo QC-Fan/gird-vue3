844da9aa511b76dad14a3255205cfefcc4f2ced6
zh2 <1024981189@qq.com>
2024-03-29 17:39:04 +0800

    'init'

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import grid from './components/grid.vue'
+
+</script>
+
+<template>
+  <grid></grid>
+</template>
+
+<style scoped></style>
---
+++ src/components/ButtonRenderer.vue
@@ -0,0 +1,16 @@
+<template>
+    <el-button type="primary" @click="handleButtonClick(params)">新增</el-button>
+</template>
+<script setup>
+function handleButtonClick(params) {
+    console.log(params);
+    // params.colDef.colSpan = row => {
+    //     return 6;
+    // };
+    // 合并第2列和第3列
+
+
+
+}
+</script>
+<style scoped></style>
